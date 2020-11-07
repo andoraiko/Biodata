dcd24cc0add75076149fb1bad9b09c92575ba215
andoraiko <74086283+andoraiko@users.noreply.github.com>
2020-11-07 11:53:42 +0700

    Update index.php

--- app_login_register/index.php
+++ app_login_register/index.php
@@ -1,73 +1,35 @@
-<?php 
-session_start();
-if(! isset($_SESSION['is_login']))
-{
-  header('location:login.php');
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Biodata</title>
+<style>
+.wrap{
+background-color:rgba(255,255,255,0.7);
+width: 800px;
+color:black;
+margin: 20px auto;
+padding:15px;
 }
-?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v3.8.5">
-    <title>Starter Template · Bootstrap</title>
- 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/starter-template/">
- 
-    <!-- Bootstrap core CSS -->
-<link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
- 
- 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
- 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-    <!-- Custom styles for this template -->
-    <link href="assets/css/starter-template.css" rel="stylesheet">
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-  <img class="mb-0" src="assets/css/Eresha.PNG" alt="" width="50" height="50">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
- 
-  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-    
- 
-    <a href="logout.php" class="form-inline my-2 my-lg-0 btn btn-secondary">Logout</a>
- 
-  </div>
-</nav>
- 
-<main role="main" class="container">
- 
-  <div class="starter-template">
-    <h1>Selamat Datang <?php echo $_SESSION['nama']; ?></h1>
-    <p class="lead">TEKNIK INFORMATIKA - STMIK ERESHA</p>
-  </div>
- 
-</main><!-- /.container -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script>window.jQuery || document.write('<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')</script><script src="/docs/4.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script></body>
+</style>
+</head>
+<body style="background: url(URL-GAMBAR-BACKGROUND.jpg);background-size:cover;background-attachment: fixed;">
+<div class="wrap">
+<h1 class="class" align="center">BIODATA DIRI</h1><hr/ align="center" width="25%" style="border-top: 3px double #8c8b8b;">
+<table>
+<tr><td rowspan="10" width="100px">
+<img src="URL-FOTOMU.jpg" width="200px" style="display: block;border-radius: 50%;border-color:white;margin-right:30px" border="2px" ></td></tr>
+<tr><td><b>Nama</b></td><td>:</td> <td>Helmi Irfansah</td></tr>
+<tr><td><b>NIM</b></td><td>:</td> <td>57</td></tr>
+<tr><td><b>Alamat</b></td><td>:</td> <td>RUMAH</td></tr>
+<tr><td><b>TTL</b></td><td>:</td> <td>Kebumen, 02 Juli 1999</td></tr>
+<tr><td><b>Email</b></td><td>:</td> <td>[email protected]</td></tr>
+<tr><td><b>Hobi</b></td><td>:</td> <td>Bermain Game</td></tr>
+<tr><td><b>Cita-cita</b></td><td>:</td> <td>Webmaster</td></tr></table>
+<table>
+<h3>Riwayat Pendidikan</h3>
+<tr><td><b>SD</b></td><td>:</td> <td>SDN 1 Terserah</td></tr>
+<tr><td><b>SMP</b></td><td>:</td> <td>SMPN 1 Terserah</td></tr>
+<tr><td><b>SMK</b></td><td>:</td> <td>SMKN 1 Terserah</td></tr></table>
+<div>
+</body>
 </html>
